6177d34b67a576fdd11d5a5f30366ed4422a61d5
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2022-07-22 21:16:19 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using CSharpDotNet;
+using Projects;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
